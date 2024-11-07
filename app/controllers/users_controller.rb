55ff0da8fd86135abf93220d302385561b45eeee
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-11-06 16:01:56 -0800

    Add routes for user, create user controller, and migrate db (fix)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    if params[:id]
+      @user = User.find(params[:id])
+    else
+      @user = current_user
+    end
+  end
+end
