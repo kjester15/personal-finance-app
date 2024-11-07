c62e46d71f5bda70bcc232274f75e8a7c6a7dfcd
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-11-06 16:48:17 -0800

    Complete prelim import controller

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,2 +1,49 @@
 class ImportsController < ApplicationController
+  def index
+    @imports = Import.all
+  end
+
+  def show
+    @import = Import.find(params[:id])
+  end
+
+  def new
+    @import = Import.new
+  end
+
+  def create
+    @import = current_user.imports.build(import_params)
+
+    if @import.save
+      redirect_to imports_path(@import.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # def edit
+  #   @import = Import.find(params[:id])
+  # end
+
+  # def update
+  #   @import = Import.find(params[:id])
+
+  #   if @import.update(import_params)
+  #     redirect_to posts_path(@import.id)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+
+  def destroy
+    @import = Import.find(params[:id])
+    @import.destroy
+    redirect_to :root
+  end
+
+  private
+
+  def import_params
+    params.require(:import).permit(:file)
+  end
 end
