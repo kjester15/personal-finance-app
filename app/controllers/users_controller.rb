c1ded6b1d5769b636570c1dbcfcc5550029751ce
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-11-12 17:41:13 -0800

    Create user partial and prelim import render in user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    if params[:id]
-      @user = User.find(params[:id])
-    else
-      @user = current_user
-    end
+    @user = current_user
   end
 end
