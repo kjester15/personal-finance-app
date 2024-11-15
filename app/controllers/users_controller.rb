d4c917d941d9954106ed27666c5092eddf22ca41
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-11-15 15:54:58 -0800

    Complete prelim card sum in profile show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,6 @@ class UsersController < ApplicationController
     @categories = current_user.imports.select(:category).distinct.order(category: :asc)
     @dates = @user.imports.select(:date).distinct.order(date: :asc)
     @vendors = @user.imports.select(:vendor).distinct.order(vendor: :asc)
+    @cards = @user.imports.select(:card).distinct.order(card: :asc)
   end
 end
