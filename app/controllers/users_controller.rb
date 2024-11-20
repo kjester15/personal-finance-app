344d1eb7baf127d1e6da5d41b8e06ff85fd7458f
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-11-19 16:05:51 -0800

    Add non-functional select import dropdown to user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,19 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+    @options = []
+    current_user.imports.select(:name).distinct.order(name: :asc).each do |import|
+      @options << import.name
+    end
     @categories = current_user.imports.select(:category).distinct.order(category: :asc)
     @dates = @user.imports.select(:date).distinct.order(date: :asc)
     @vendors = @user.imports.select(:vendor).distinct.order(vendor: :asc)
     @cards = @user.imports.select(:card).distinct.order(card: :asc)
+  end
+
+  private
+
+  def import_params
+    params.require(:import).permit(:file, :name)
   end
 end
